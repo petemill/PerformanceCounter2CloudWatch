b6b812c6204cb9fec4b46820e2fbfb7c1e14c063
Pete Miller <petemill@live.com>
2011-10-06 23:35:38 +0100

    Initial PerformanceCounter data provider. Will be configuration based. Hard-coded to get the local machine's total cpu usage at the moment.

---
+++ Class Libraries/Natol.PerformanceCounter2CloudWatch.PerformanceCounters/PerformanceCounterDescriptor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Natol.PerformanceCounter2CloudWatch.Framework;
+using System.Diagnostics;
+
+namespace Natol.PerformanceCounter2CloudWatch.PerformanceCounters
+{
+    public class PerformanceCounterDescriptor : CounterDescriptor
+    {
+        public PerformanceCounter SystemCounter { get; set; }
+
+        public override double? GetCount()
+        {
+            try
+            {
+                return Convert.ToDouble(SystemCounter.NextValue() / Environment.ProcessorCount);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ Class Libraries/Natol.PerformanceCounter2CloudWatch.PerformanceCounters/PerformanceCounterLister.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Natol.PerformanceCounter2CloudWatch.Framework;
+using System.Diagnostics;
+
+namespace Natol.PerformanceCounter2CloudWatch.PerformanceCounters
+{
+    public class PerformanceCounterLister : IPerformanceCounterLister
+    {
+        public IList<CounterDescriptor> UpdateCounterItems(IList<CounterDescriptor> counterItems)
+        {
+            //no need to update, since we're hardcoded (& eventually configuration-vased)
+            if (counterItems.Count == 0)
+            {
+                var pc = new PerformanceCounter("Processor", "% Processor Time", "_Total", true);
+                var result = new PerformanceCounterDescriptor
+                {
+                    Name = "CPUUtilization",
+                    SystemCounter = pc,
+                    Unit = "Percent",
+                    MetricName = "CPUUtilization"
+                };
+                counterItems.Add(result);
+            }
+
+            return counterItems;
+        }
+    }
+}
