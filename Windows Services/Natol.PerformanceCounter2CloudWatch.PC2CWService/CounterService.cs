b1f7a424a2dc4a6a2827d3320318d90cf0cb2de4
Pete Miller <petemill@live.com>
2012-02-14 19:16:49 -0800

    Windows Service (basic dependency injection so far). Note that amazon credentials are fake and will not work - replace with your own.

---
+++ Windows Services/Natol.PerformanceCounter2CloudWatch.PC2CWService/CounterService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using Natol.PerformanceCounter2CloudWatch.Framework;
+using Natol.PerformanceCounter2CloudWatch.IIS;
+using Autofac;
+
+namespace Natol.PerformanceCounter2CloudWatch.PC2CWService
+{
+    public partial class CounterService : ServiceBase
+    {
+        public CounterService()
+        {
+            InitializeComponent();
+        }
+
+        //service-wide reference to counter-manager
+        CounterManager manager;
+
+        protected override void OnStart(string[] args)
+        {
+            try
+            {
+                //setup dependencies
+                var builder = new ContainerBuilder();
+                Console.WriteLine("Setting up dependencies");
+
+                //todo: configuration based provider setup
+                builder.Register<IisServerWorkerProcessCpuLister>(c => new IisServerWorkerProcessCpuLister()).As<IPerformanceCounterLister>();
+
+
+                //setup manager
+                manager = new CounterManager(builder);
+                //todo: lof ro dilw bt setting a function on manager.WriteToLog
+                manager.Start();
+            }
+            catch 
+            {
+                //todo: log exception setting up
+            }
+        }
+
+        protected override void OnStop()
+        {
+            try
+            {
+                if (manager != null)
+                    manager.Stop();
+            }
+            catch
+            {
+                //todo: log eception stopping
+            }
+        }
+    }
+}
